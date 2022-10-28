f363207a33b7cb692739c8b0a106941a1b4d1e9d
Assad Isah <nottherealalanturing@gmail.com>
2022-10-28 09:50:44 +0100

    Update messages_controller.rb

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,6 +1,6 @@
 class API::V1::MessagesController < ApplicationController
   def index
-    @message = Message.find(rand(1..5))
+    @message = Message.all.sample
 
     render json: @message
   end
